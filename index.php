cfbde5d30f5c07dea6aa9ba1146fdced851374ab
meduza <meduza@.com>
2016-11-09 20:14:37 +0200

    Add files

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Gallery</title>
+        <link rel="shortcut icon" href="favicon.png" type="image/x-icon"/>
+        <link href="css/main.css" rel="stylesheet" type="text/css"/>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="js/script.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <form action="addphoto.php" method="post" enctype="multipart/form-data">
+            <input type="file" name="photo"/>
+            <input type="submit"/>
+        </form>
+        <div class="gallery"></div>
+    </body>
+</html>
